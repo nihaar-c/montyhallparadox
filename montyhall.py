1d0d4d99755590150c26706664d621e149e60fd3
nihaar-c <charagulla.nihaar@gmail.com>
2023-11-06 15:27:33 -0500

    Generalized to n doors

--- montyhall.py
+++ montyhall.py
@@ -6,38 +6,41 @@ winSim1 = 0
 #case that user stays
 winSim2 = 0
 
+n = random.randint(3,100)
+
+# n = 3. can use this to check percentages
+
 for i in range(5000):
-    carIdx = random.randint(0,2)
-    choice = random.randint(0,2)
+    carIdx = random.randint(0,n-1)
+    choice = random.randint(0,n-1)
 
 
     #choose a door to eliminate
-    doors = [0,1,2]
+    doors = [j for j in range(n)]
     del doors[choice]
     if carIdx in doors:
         doors.remove(carIdx)
     removedIdx = random.choice(doors)
 
-    doors = [0,1,2]
+    doors = [j for j in range(n)]
     del doors[removedIdx]
-    # 2 doors remaining
+    # one door removed
 
     if choice == carIdx:
+        #no switching
         winSim2 += 1
     else:
+        #switch.. so remove user's choice and randomly choose from remaining
         doors.remove(choice)
-        choice = doors[0]
+        choice = random.choice(doors)
         if carIdx == choice:
             winSim1 += 1
     
 
+print (str(n) + " doors")
 
 #prints a value close to 2/3
 print("Odds that switching doors wins: " + str(100*float(winSim1/5000)) + "%.")
     
 #prints a value close to 1/3
 print("Odds that not switching doors wins: " + str(100*float(winSim2/5000)) + "%.")
-
-
-
-
